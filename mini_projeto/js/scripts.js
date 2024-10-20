f9d027de1fbb6016ed3aec4c99d144023b09c662
MartaCCaetano <marta.cb.caetano@gmail.com>
2024-10-20 21:13:34 +0100

    j

--- mini_projeto/js/scripts.js
+++ mini_projeto/js/scripts.js
@@ -130,7 +130,8 @@ function buildCharacter(classCharacter, proficiencies, equipment, subClasses){
                         <p id="proficiencies" class="card-text"><b>Proficiency: </b>${proficiencies}</p>
                         <p id="equipment" class="card-text"><b>Equipment: </b>${equipment}</p>
                         <p id=" " class="card-text"><b>SubClass: </b>${subClasses}</p>
-                    </div>`; 
+                    </div>
+                    <br>`; 
 }
 
 fetch("https://www.dnd5eapi.co"+classesurl).then(function (res) {
