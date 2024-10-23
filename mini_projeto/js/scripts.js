e076ffdcb0edbc3c77c7c2169aa55c5f68c74c36
MartaCCaetano <marta.cb.caetano@gmail.com>
2024-10-23 10:20:06 +0100

    correção assets error

--- mini_projeto/js/scripts.js
+++ mini_projeto/js/scripts.js
@@ -121,7 +121,7 @@ fetch(urlall).then(function (res) {
 
                 //Guarda na variável "classesurl" o url da classe na posição random do allClasses
                 let classesurl = allclasses[random];
-                console.log(classesurl);
+                //console.log(classesurl);
                 //console.log(random);
             
 const cards = document.getElementById('characters');
